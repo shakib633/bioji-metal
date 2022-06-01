54a88733489968a4d5c228fe43cd543bbbb0d730
shakiv633 <shakibshuvo633@gmail.com>
2022-06-02 03:37:35 +0600

    update

--- src/pages/hooks/UseToken.js
+++ src/pages/hooks/UseToken.js
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const useToken = user => {
-    const [token, setToken] = useState('');
+const useToken = (user) => {
+  const [token, setToken] = useState("");
 
-    useEffect(() => {
-         const email = user?.user?.email;
-        const currentUser = { email:email }
-        if (email) {
-            const url = `https://ancient-ravine-57330.herokuapp.com/user/${email}`;
-            fetch(url, {
-                method: 'PUT',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify(currentUser)
-            })
-                .then(res => res.json())
-                .then(data => {
-                    const accessToken = data.token;
-                    localStorage.setItem('accessToken', accessToken);
-                    setToken(accessToken);
-                })
-        }
-    }, [user]);
-    return [token, setToken];
-}
+  useEffect(() => {
+    const email = user?.user?.email;
+    const currentUser = { email: email };
+    if (email) {
+      const url = `https://ancient-ravine-57330.herokuapp.com/user/${email}`;
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(currentUser),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const accessToken = data.token;
+          localStorage.setItem("accessToken", accessToken);
+          setToken(accessToken);
+        });
+    }
+  }, [user]);
+  return [token];
+};
 export default useToken;
