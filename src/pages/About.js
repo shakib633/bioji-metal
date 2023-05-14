b9179e4823156c8d7cbdb07d32409436576133c9
shakiv633 <shakibshuvo633@gmail.com>
2023-05-15 01:01:20 +0600

    ok

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div className="p-2 m-2 border-none shadow-2xl">
-            <h3 className="text-2xl font-bold text-info">About us</h3>
-            <p>Bioji Lyne Metals Co.,ltd is specialized in producing,marketing and new product manufacturing,it has been
-well known as an integrated enterprise,the company is relies on the "China Titanium Valley"as a strong comprehensive
-strength. efficient resource sharing; With unique geographical, material, scientific research advantage, to provide users
-with a large number of high quality rare metal products. 
-Our company is Professional in : Titanium, Nickel,and non-ferrous metal seamless tube, pipe fittings, pipeline,
-equipment, containers, castings, forgings, standard parts and the plate, rod, wire.
-Processing and sales: Tungsten, Molybdenum, Tantalum, Niobium and other rare metal tube, plate, bar,
-wire and nitinol memory alloy, nickel and nickel alloys, titanium and titanium alloy equipment and
-other non-standard equipment.
-<p className="text-center mx-10"><img src="http://www.ly-metal.com/pic/other/2019-03-28-02-09-339.jpg"></img>
-</p>
-Our Products are exported to : Italy ,Sweden, Iran, Germany, Finland , Israel, the United States, Canada,
-South Korea, India, Malaysia and some other European countries.</p>
-        </div>
-    );
+  return (
+    <div className="p-2 m-2 border-none shadow-2xl">
+      <h3 className="text-2xl font-bold text-info">About us</h3>
+      <p>
+        Bioji Lyne Metals Co.,ltd is specialized in producing,marketing and new
+        product manufacturing,it has been well known as an integrated
+        enterprise,the company is relies on the "China Titanium Valley"as a
+        strong comprehensive strength. efficient resource sharing; With unique
+        geographical, material, scientific research advantage, to provide users
+        with a large number of high quality rare metal products. Our company is
+        Professional in : Titanium, Nickel,and non-ferrous metal seamless tube,
+        pipe fittings, pipeline, equipment, containers, castings, forgings,
+        standard parts and the plate, rod, wire. Processing and sales: Tungsten,
+        Molybdenum, Tantalum, Niobium and other rare metal tube, plate, bar,
+        wire and nitinol memory alloy, nickel and nickel alloys, titanium and
+        titanium alloy equipment and other non-standard equipment.
+        {/* <div className="flex">
+          <img src="http://www.ly-metal.com/pic/other/2019-03-28-02-09-339.jpg"></img>
+        </div> */}
+        Our Products are exported to : Italy ,Sweden, Iran, Germany, Finland ,
+        Israel, the United States, Canada, South Korea, India, Malaysia and some
+        other European countries.
+      </p>
+    </div>
+  );
 };
 
 export default About;
